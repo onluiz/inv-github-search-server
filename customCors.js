25ff93d1c121000433ad16e455b609f0a45fb53f
Luiz Alberto <luiz.alberto1723@gmail.com>
2018-02-01 21:58:36 -0200

    Commit com a estrutura inicial do projeto

---
+++ customCors.js
@@ -0,0 +1,9 @@
+exports.allow = (req, res, next) => {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Accept,X-Requested-With');
+  if (req.method!='OPTIONS')
+    return next();
+  res.send(204);
+}
---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express'),
+  bodyParser = require('body-parser'),
+  customCors = require('./customCors'),
+  axios = require('axios');
+
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(customCors.allow);
+
+//REFATORAR extractToken
+const extractToken = (data) => {
+  return data.split('=')[1].split('&')[0];
+}
+
+const CLIENT_ID = '2a4d09cfa80c8fdce2b6';
+const CLIENT_SECRET = '43de0231e10c595f8310d373f644859112ed900a';
+
+const requestToken = (code, callback) => {
+  axios.post('https://github.com/login/oauth/access_token', {
+    code,
+    client_id: CLIENT_ID,
+    client_secret: CLIENT_SECRET
+  })
+  .then(res => {
+    const token = extractToken(res.data);
+    callback(token);
+  })
+  .catch(err => {
+    callback(err);
+  });
+}
+
+app.get('/login/github/return', function(req, res) {
+  const code = req.param('code');
+  req.
+  res.redirect(`http://localhost:8080/#/home/${code}`);
+});
+
+app.get('/login/github/token', function(req, res) {
+  const code = req.param('code');
+  requestToken(code, function(token) {
+    res.json({ token });
+  })
+})
+
+console.log('app is running on 3000');
+app.listen(3000);
